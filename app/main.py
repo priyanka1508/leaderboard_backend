45253f6aa43a269596ea98772c5340c2e39fffbd
priyanka1508 <priyankaphulwari16@gmail.com>
2025-07-24 22:21:34 +0530

    updating new relic env vars

--- app/main.py
+++ app/main.py
@@ -1,24 +1,33 @@
+import os
 import newrelic.agent
-newrelic.agent.initialize('newrelic.ini')
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from newrelic.api.asgi_application import ASGIApplicationWrapper
 
-# ✅ Define app first
-app = FastAPI()
 
+os.environ["NEW_RELIC_APP_NAME"] = os.getenv("NEW_RELIC_APP_NAME")
+os.environ["NEW_RELIC_LICENSE_KEY"] = os.getenv("NEW_RELIC_LICENSE_KEY")
+os.environ["NEW_RELIC_LOG"] = "stdout"
+os.environ["NEW_RELIC_DISTRIBUTED_TRACING_ENABLED"] = "true"
+os.environ["NEW_RELIC_ENABLED"] = "true"
+
+newrelic.agent.initialize()
+
+app = FastAPI()
 app = ASGIApplicationWrapper(app)
 
-# ✅ Add CORS middleware after app is created
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "https://leaderboard-ui-murex.vercel.app/"],  # adjust based on your frontend
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:3002",
+        "https://leaderboard-ui-murex.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Then import and include your routers
 from app.routes import router
 app.include_router(router, prefix="/api/leaderboard")
