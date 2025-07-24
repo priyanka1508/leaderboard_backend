5dc0ff5a5948935d33a459513c6ffad29acf8e4f
priyanka1508 <priyankaphulwari16@gmail.com>
2025-07-24 22:34:21 +0530

    updating origins to mitigate CORS

--- app/main.py
+++ app/main.py
@@ -19,10 +19,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://localhost:3002",
-        "https://leaderboard-ui-murex.vercel.app"
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
