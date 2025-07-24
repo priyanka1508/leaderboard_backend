ef5bd270e4897fd7c98bf397bd171e74d88125a5
priyanka1508 <priyankaphulwari16@gmail.com>
2025-07-24 22:23:33 +0530

    removing new-relic integration

--- app/main.py
+++ app/main.py
@@ -2,19 +2,19 @@ import os
 import newrelic.agent
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from newrelic.api.asgi_application import ASGIApplicationWrapper
+# from newrelic.api.asgi_application import ASGIApplicationWrapper
 
 
-os.environ["NEW_RELIC_APP_NAME"] = os.getenv("NEW_RELIC_APP_NAME")
-os.environ["NEW_RELIC_LICENSE_KEY"] = os.getenv("NEW_RELIC_LICENSE_KEY")
-os.environ["NEW_RELIC_LOG"] = "stdout"
-os.environ["NEW_RELIC_DISTRIBUTED_TRACING_ENABLED"] = "true"
-os.environ["NEW_RELIC_ENABLED"] = "true"
+# os.environ["NEW_RELIC_APP_NAME"] = os.getenv("NEW_RELIC_APP_NAME")
+# os.environ["NEW_RELIC_LICENSE_KEY"] = os.getenv("NEW_RELIC_LICENSE_KEY")
+# os.environ["NEW_RELIC_LOG"] = "stdout"
+# os.environ["NEW_RELIC_DISTRIBUTED_TRACING_ENABLED"] = "true"
+# os.environ["NEW_RELIC_ENABLED"] = "true"
 
-newrelic.agent.initialize()
+# newrelic.agent.initialize()
 
 app = FastAPI()
-app = ASGIApplicationWrapper(app)
+# app = ASGIApplicationWrapper(app)
 
 app.add_middleware(
     CORSMiddleware,
